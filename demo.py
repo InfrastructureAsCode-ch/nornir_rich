c169803968e5dd15b6fd3e76494e0c007109c4ea
ubaumann <github@m.ubaumann.ch>
2020-11-01 23:55:33 +0100

    init

---
+++ demo.py
@@ -0,0 +1,83 @@
+import logging
+from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_rich.functions import print_result
+
+from random import randrange
+
+
+nr = InitNornir(
+    runner={
+        "plugin": "threaded",
+        "options": {
+            "num_workers": 100,
+        },
+    },
+    inventory={
+        "plugin": "SimpleInventory",
+        "options": {
+            "host_file": "demo_inventory/hosts.yaml",
+            "group_file": "demo_inventory/groups.yaml"
+        },
+    },
+)
+
+def hello_world(task: Task) -> Result:
+    return Result(
+        host=task.host,
+        result=f"{task.host.name} says hello world!"
+    )
+
+
+def say(task: Task, text: str) -> Result:
+    return Result(
+        host=task.host,
+        result=f"{task.host.name} says {text}"
+    )
+
+def count(task: Task, number: int) -> Result:
+    if randrange(5) == 4:
+        raise Exception("Random exception") 
+    return Result(
+        host=task.host,
+        result=f"{[n for n in range(0, number)]}"
+    )
+
+def greet_and_count(task: Task, number: int) -> Result:
+    task.run(
+        name="Greeting is the polite thing to do",
+        task=say,
+        text="hi!",
+    )
+
+    task.run(
+        name="Counting beans",
+        task=count,
+        number=number,
+        severity_level=logging.DEBUG
+    )
+    task.run(
+        name="We should say bye too",
+        task=say,
+        text="bye!",
+    )
+
+    # let's inform if we counted even or odd times
+    even_or_odds = "even" if number % 2 == 1 else "odd"
+    return Result(
+        host=task.host,
+        result=f"{task.host} counted {even_or_odds} times!"
+    )    
+
+results = nr.run(
+    task=hello_world
+)
+
+print_result(results, expand=True)
+
+results = nr.run(
+    task=greet_and_count, number=10
+)
+
+print_result(results)
+print_result(results, vars=["diff", "result", "name", "exception", "severity_level"])
---
+++ nornir_rich/functions.py
@@ -0,0 +1,111 @@
+import logging
+import threading
+from typing import List, cast
+
+from nornir.core.task import AggregatedResult, MultiResult, Result
+
+from rich import print
+from rich.columns import Columns
+from rich.panel import Panel
+from rich.scope import render_scope
+from rich.padding import PaddingDimensions
+
+
+LOCK = threading.Lock()
+
+
+class PrintHelper:
+    def __init__(
+        self,
+        columns_settings: dict = dict(),
+        padding: PaddingDimensions = None,
+        expand: bool = False,
+        equal: bool = True,
+        vars: List[str] = None,
+        severity_level: int = None,
+        failed: bool = None,
+    ) -> None:
+        self.columns_settings = columns_settings
+        self.columns_settings["expand"] = expand
+        self.columns_settings["equal"] = equal
+        if padding:
+            self.columns_settings["padding"] = padding
+        self.vars = vars
+        self.severity_level = severity_level
+        self.failed = failed
+
+    def _print_aggregated_result(self, result: AggregatedResult) -> Panel:
+        mulit_results = [
+            self._print_multi_result(result=mulit_result, host=host)
+            for host, mulit_result in result.items()
+        ]
+        columns = Columns(mulit_results, **self.columns_settings)
+        panel = Panel(
+            columns, title=result.name, style="red" if result.failed else "green"
+        )
+        return panel
+
+    def _print_multi_result(self, result: MultiResult, host: str) -> Panel:
+        results = [self._print_result(r) for r in result if r.severity_level >= self.severity_level]
+        panel = Panel(
+            Columns(results, **self.columns_settings),
+            title=f"{host} | {result.name}",
+            style="red" if result.failed else "green",
+        )
+        return panel
+
+    def _print_result(self, result: Result) -> Panel:
+        if result.severity_level < self.severity_level:
+            return None
+        if self.vars:
+            return Panel(
+                render_scope({x: getattr(result, x) for x in self.vars}),
+                title=result.name,
+                style="red" if result.failed else "green",
+            )
+        return Panel(
+            result.result,
+            title=result.name,
+            style="red" if result.failed else "green",
+        )
+
+
+def print_result(
+    result: Result,
+    vars: List[str] = None,
+    failed: bool = False,
+    severity_level: int = logging.INFO,
+    columns_settings=dict(),
+    padding=None,
+    expand=False,
+    equal=True,
+) -> None:
+    """
+    Prints an object of type `nornir.core.task.Result`
+
+    Arguments:
+      result: from a previous task
+      vars: Which attributes you want to print
+      failed: if ``True`` assume the task failed
+      severity_level: Print only errors with this severity level or higher
+    """
+    LOCK.acquire()
+    equal = False if expand else equal
+    ph = PrintHelper(
+        columns_settings=columns_settings,
+        padding=padding,
+        expand=expand,
+        equal=equal,
+        vars=vars,
+        severity_level=severity_level,
+        failed=failed,
+    )
+    try:
+        if isinstance(result, AggregatedResult):
+            print(ph._print_aggregated_result(result))
+        elif isinstance(result, MultiResult):
+            print(ph._print_multi_result(result))
+        elif isinstance(result, Result):
+            print(ph._print_result(result))
+    finally:
+        LOCK.release()
