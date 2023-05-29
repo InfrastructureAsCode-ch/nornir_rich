80eb2c230f77637b7e96b292200c6699e79db9ca
ubaumann <github@m.ubaumann.ch>
2023-05-29 16:35:49 +0200

    Add line_breaks option to allow printing line breaks

--- nornir_rich/functions.py
+++ nornir_rich/functions.py
@@ -1,7 +1,7 @@
 import logging
 import threading
-from typing import Any, List, Union, Dict, Optional
-from rich.console import RenderableType
+from typing import Any, List, Union, Dict, Tuple, Optional
+from rich.console import RenderableType, ConsoleRenderable
 
 from nornir.core import Nornir
 from nornir.core.inventory import Inventory
@@ -14,6 +14,8 @@ from rich.scope import render_scope
 from rich.padding import PaddingDimensions
 from rich.pretty import Pretty
 from rich.protocol import is_renderable, rich_cast
+from rich.table import Table
+from rich.text import Text
 
 
 LOCK = threading.Lock()
@@ -31,6 +33,7 @@ class RichHelper:
       vars: Which attributes you want to print
       severity_level: Print only errors with this severity level or higher
       failed: if ``True`` assume the task failed
+      line_breaks: if ``True`` line breaks in strings will be printed
     """
 
     def __init__(
@@ -42,6 +45,7 @@ class RichHelper:
         vars: Optional[List[str]] = None,
         severity_level: int = 0,
         failed: Optional[bool] = None,
+        line_breaks: Optional[bool] = None,
     ) -> None:
         self.columns_settings = columns_settings
         self.columns_settings["expand"] = expand
@@ -51,6 +55,7 @@ class RichHelper:
         self.vars = vars
         self.severity_level = severity_level
         self.failed = failed
+        self.line_breaks = line_breaks
 
     def print_aggregated_result(self, result: AggregatedResult) -> Panel:
         """
@@ -110,7 +115,7 @@ class RichHelper:
             return None
         if self.vars:
             return Panel(
-                render_scope({x: getattr(result, x) for x in self.vars}),
+                self._scope_talbe(scope={x: getattr(result, x) for x in self.vars}),
                 title=result.name,
                 style="red" if result.failed else "green",
             )
@@ -129,13 +134,15 @@ class RichHelper:
     def print_scopes(self, scopes: Dict[str, Any]) -> Columns:
         if self.vars:
             columns = [
-                render_scope(
+                self._scope_talbe(
                     {k: v for k, v in map.items() if k in self.vars}, title=name
                 )
                 for name, map in scopes.items()
             ]
         else:
-            columns = [render_scope(map, title=name) for name, map in scopes.items()]
+            columns = [
+                self._scope_talbe(map, title=name) for name, map in scopes.items()
+            ]
         return Columns(
             columns,
             **self.columns_settings,
@@ -161,6 +168,54 @@ class RichHelper:
             return self.print_result(result)
         return Panel(f"Unable to find printer function for {result}")
 
+    def _scope_talbe(
+        self,
+        scope: "Dict[str, Any]",
+        title: Optional[str] = None,
+    ) -> Panel:
+        """Render python variables in a given scope.
+
+        ***This code is heavily inspired/copied by/from nornir.scope.render_scope***
+        https://github.com/Textualize/rich/blob/master/rich/scope.py
+
+        Args:
+            scope (Dict[str, Any]): A mapping containing variable names and values.
+
+        Returns:
+            Panel: Panel containing the table with key value mapping
+        """
+        items_table = Table.grid(padding=(0, 1), expand=False)
+        items_table.add_column(justify="right")
+
+        def sort_items(item: Tuple[str, Any]) -> Tuple[bool, str]:
+            """Sort special variables first, then alphabetically."""
+            key, _ = item
+            return (not key.startswith("__"), key.lower())
+
+        items = sorted(scope.items(), key=sort_items)
+
+        for key, value in items:
+            key_text = Text.assemble(
+                (key, "scope.key.special" if key.startswith("__") else "scope.key"),
+                (" =", "scope.equals"),
+            )
+
+            if self.line_breaks and isinstance(value, str):
+                value_text: ConsoleRenderable = Text(value.strip())
+            else:
+                value_text = Pretty(value)
+
+            items_table.add_row(
+                key_text,
+                value_text,
+            )
+        return Panel.fit(
+            items_table,
+            title=title,
+            border_style="scope.border",
+            padding=(0, 1),
+        )
+
 
 def print_result(
     result: Union[Result, MultiResult, AggregatedResult],
@@ -171,6 +226,7 @@ def print_result(
     padding: Optional[PaddingDimensions] = None,
     expand: bool = False,
     equal: bool = True,
+    line_breaks: bool = False,
 ) -> None:
     """
     Prints an object of type `nornir.core.task.Result` || `nornir.core.task.MultiResult` || `nornir.core.task.AggregatedResult`
@@ -184,6 +240,7 @@ def print_result(
       padding: Optional padding around cells. Defaults to (0, 1).
       expand: Expand columns to full width. Defaults to False.
       equal: Equal sized columns. Defaults to False
+      line_breaks: if ``True`` line breaks in strings will be printed
     """
     LOCK.acquire()
     equal = False if expand else equal
@@ -195,6 +252,7 @@ def print_result(
         vars=vars,
         severity_level=severity_level,
         failed=failed,
+        line_breaks=line_breaks,
     )
     try:
         if isinstance(result, AggregatedResult):
@@ -216,6 +274,7 @@ def print_failed_hosts(
     padding: Optional[PaddingDimensions] = None,
     expand: bool = False,
     equal: bool = True,
+    line_breaks: bool = False,
 ) -> None:
     """
     Prints results of all failed hosts from `nornir.core.task.AggregatedResult`
@@ -229,6 +288,7 @@ def print_failed_hosts(
       padding: Optional padding around cells. Defaults to (0, 1).
       expand: Expand columns to full width. Defaults to False.
       equal: Equal sized columns. Defaults to False
+      line_breaks: if ``True`` line breaks in strings will be printed
     """
     LOCK.acquire()
     equal = False if expand else equal
@@ -240,6 +300,7 @@ def print_failed_hosts(
         vars=vars,
         severity_level=severity_level,
         failed=failed,
+        line_breaks=line_breaks,
     )
     try:
         for host, multi_result in result.failed_hosts.items():
